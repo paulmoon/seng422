698615c7a7d9a32f5b221821280ac85a9211eae9
Danny <guan3d@gmail.com>
2014-07-14 15:22:37 -0700

    finished login page

--- www/app/scripts/controllers/login.js
+++ www/app/scripts/controllers/login.js
@@ -2,10 +2,10 @@
 
 angular.module('angWeatherAppApp')
   .controller('LoginCtrl', function ($scope, $http, setting) {
-  	var url = setting.apiurl + '/login'; 
+  	var url = setting.apiurl + '/verify_credentials'; 
   	$scope.submit = function(){
   		var sendData = {
-  			"name": $scope.username,
+  			"usernamename": $scope.username,
   			"password": $scope.password
   		};
   		$http.post(url, sendData)
