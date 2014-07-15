4d856ad969113c84195d90473106a8137c7a7e32
Danny <guan3d@gmail.com>
2014-07-15 02:38:02 -0700

    Added cookie stores for caching tokens in browsers, I think we need a google doc of all the api endpoints, types, and data/parameter inputs

--- www/app/scripts/controllers/login.js
+++ www/app/scripts/controllers/login.js
@@ -1,17 +1,17 @@
 'use strict';
 
 angular.module('angWeatherAppApp')
-  .controller('LoginCtrl', function ($scope, $http, setting) {
-  	var url = setting.apiurl + "/verify_credentials"; 
+  .controller('LoginCtrl', function ($scope, $http, setting, $cookieStore) {
+  	var url = setting.apiurl + "/verify_credentials";
   	$scope.submit = function(){
   		var sendData = {
   			"username": $scope.username,
   			"password": $scope.password
   		};
-
   		$http.post(url, sendData)
   		.success(function(data){
-  			console.log(data);
+  			$cookieStore.put('angWeatherToken', data.token);
+  			console.log($cookieStore.get("angWeatherToken"));
   		})
   		.error(function(data) {
         	console.log('Error: ' + data);
