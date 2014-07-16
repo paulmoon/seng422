b39c2d151398af14480b2e671e21713cfb3d45c4
Danny <guan3d@gmail.com>
2014-07-15 17:25:19 -0700

    added basic login and redirect services to the application

--- www/app/scripts/controllers/login.js
+++ www/app/scripts/controllers/login.js
@@ -1,8 +1,25 @@
 'use strict';
 
 angular.module('angWeatherAppApp')
-  .controller('LoginCtrl', function ($scope, $http, setting, $cookieStore) {
+  .controller('LoginCtrl', function ($scope, $http, setting, $cookieStore, $location) {
   	var url = setting.apiurl + "/verify_credentials";
+  	var authorize_url = setting.apiurl + "/authorize_token/";
+  	var authorize_token = 'Token ' + $cookieStore.get("angWeatherToken");
+  	$http.defaults.headers.common.Authorization = authorize_token;
+  	$http.get(authorize_url)
+  	.success(function(data){
+  		console.log(data);
+  		if(data.role == 0){
+  			$location.path("/main");
+  		} else if(data.role == 1){
+  			$location.path("/main");
+  		} else if(data.role == 2) {
+  			$location.path("/main");
+  		}
+  	})
+  	.error(function(data){
+  		console.log(data);
+  	});
   	$scope.submit = function(){
   		var sendData = {
   			"username": $scope.username,
@@ -11,7 +28,7 @@ angular.module('angWeatherAppApp')
   		$http.post(url, sendData)
   		.success(function(data){
   			$cookieStore.put('angWeatherToken', data.token);
-  			console.log($cookieStore.get("angWeatherToken"));
+  			$location.path("/main");
   		})
   		.error(function(data) {
         	console.log('Error: ' + data);
