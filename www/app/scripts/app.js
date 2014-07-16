bd8c2b2a624891246883ed7c2048d4253df50dbf
Danny <guan3d@gmail.com>
2014-07-16 03:31:17 -0700

    Changed the login functionality code to use angulars factory pattern still need to add additional redirects

--- www/app/scripts/app.js
+++ www/app/scripts/app.js
@@ -9,6 +9,33 @@ angular
     'mm.foundation'
   ])
   .constant('setting', {apiurl: 'http://localhost:8000'})
+  .factory('appAuthorize', function ($cookieStore, $http, setting){
+      return {
+        login: function (credentials) {
+            var url = setting.apiurl + "/verify_credentials";
+            $http.post(url, credentials)
+            .success(function(data){
+              $cookieStore.put("angWeatherToken", data.token);
+              console.log("Added token to cookie store");
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+        },
+        isLoggedIn: function () {
+          var url = setting.apiurl + "/authorize_token/",
+          currentToken = $cookieStore.get("angWeatherToken"),
+          authorize_token = "Token " + currentToken;
+          if(currentToken === undefined) {
+            console.log("currentToken is undefined");
+            return false;
+          } else {
+            $http.defaults.headers.common.Authorization = authorize_token;
+            return true;
+          }
+        }
+      }
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- www/app/scripts/controllers/login.js
+++ www/app/scripts/controllers/login.js
@@ -1,37 +1,52 @@
 'use strict';
 
 angular.module('angWeatherAppApp')
-  .controller('LoginCtrl', function ($scope, $http, setting, $cookieStore, $location) {
-  	var url = setting.apiurl + "/verify_credentials";
-  	var authorize_url = setting.apiurl + "/authorize_token/";
-  	var authorize_token = 'Token ' + $cookieStore.get("angWeatherToken");
-  	$http.defaults.headers.common.Authorization = authorize_token;
-  	$http.get(authorize_url)
-  	.success(function(data){
-  		console.log(data);
-  		if(data.role == 0){
-  			$location.path("/main");
-  		} else if(data.role == 1){
-  			$location.path("/main");
-  		} else if(data.role == 2) {
-  			$location.path("/main");
-  		}
-  	})
-  	.error(function(data){
-  		console.log(data);
-  	});
+  .controller('LoginCtrl', function ($scope, $http, setting, $cookieStore, $location, appAuthorize) {
+  	// var url = setting.apiurl + "/verify_credentials";
+  	var url = setting.apiurl + "/authorize_token/";
+  	// var authorize_token = 'Token ' + $cookieStore.get("angWeatherToken");
+  	// $http.defaults.headers.common.Authorization = authorize_token;
+  	// $http.get(authorize_url)
+  	// .success(function(data){
+  	// 	console.log(data);
+  	// 	if(data.role == 0){
+  	// 		$location.path("/main");
+  	// 	} else if(data.role == 1){
+  	// 		$location.path("/main");
+  	// 	} else if(data.role == 2) {
+  	// 		$location.path("/main");
+  	// 	}
+  	// })
+  	// .error(function(data){
+  	// 	console.log(data);
+  	// });
+	if(appAuthorize.isLoggedIn() == true) {
+		console.log("hi1");
+		$http.get(url)
+		.success(function(data){
+			if(data.role == 0) {
+				$location.path("/main");
+			}
+		})
+		.error(function(data){
+			console.log(data);
+		});
+	} else {
+		console.log("hi2");
+	}
   	$scope.submit = function(){
   		var sendData = {
   			"username": $scope.username,
   			"password": $scope.password
   		};
-  		$http.post(url, sendData)
-  		.success(function(data){
-  			$cookieStore.put('angWeatherToken', data.token);
-  			$location.path("/main");
-  		})
-  		.error(function(data) {
-        	console.log('Error: ' + data);
-      });
+  		appAuthorize.login(sendData);
+	  		// $http.post(url, sendData)
+  		// .success(function(data){
+  		// 	$cookieStore.put('angWeatherToken', data.token);
+  		// 	$location.path("/main");
+  		// })
+  		// .error(function(data) {
+    //     	console.log('Error: ' + data);
+    //   });
   	}
   });
