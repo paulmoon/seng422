b5bbcf91783e213fdffb6663136c24dcc731a4d8
Justin Guze <jaguze@gmail.com>
2014-07-22 10:56:38 -0700

    Header is appearing in the admin view now

--- www/app/scripts/controllers/admin.js
+++ www/app/scripts/controllers/admin.js
@@ -2,12 +2,13 @@
 
 angular.module('lscsClientApp')
   .controller('AdminCtrl', function ($scope, appAuthorize, $location, setting, $http) {
-  	if(appAuthorize.isLoggedIn() == true){
+        var url = setting.apiurl + "/authorize_token/";
+        if(appAuthorize.isLoggedIn() == true){
   		$http.get(url)
   		.success(function(data){
-  			if(data.role == 0){
+  			if(data.role == 1){
   				$location.path("/manager");
-  			} else if(data.role == 1){
+  			} else if(data.role == 2){
   				$location.path("/surveyor");
   			}
   		})
