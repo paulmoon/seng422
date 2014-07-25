ab225f513bc5ebb298a22d2fb11e8bb04eaa49ed
Danny <guan3d@gmail.com>
2014-07-25 14:18:24 -0700

    made the directive work properly

--- www/app/scripts/controllers/manager.js
+++ www/app/scripts/controllers/manager.js
@@ -77,13 +77,20 @@ angular.module('lscsClientApp')
 	    woeid: '',
 	    unit: 'f',
 	    success: function(weather) {
-	    	console.log(weather);
+	      var html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+	      // html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+	      // html += '<li class="currently">'+weather.currently+'</li>';
+	      // html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+	  
+	      angular.element("#weather").html(html);
+	      console.log(weather);
 	    },
 	    error: function(error) {
 	      console.log(error);
 	    }
 	  });
-	}
+	},
+	template: "<div id='weather'></div>"
 	}
 });
 
