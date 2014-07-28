7784b25a5e47cec683fa41e26a74c8e49a1aa730
Justin Guze <jaguze@gmail.com>
2014-07-27 19:40:31 -0700

    Added bootstrap and stripped foundation

--- www/app/scripts/directives/employeecard.js
+++ www/app/scripts/directives/employeecard.js
@@ -13,9 +13,6 @@ angular.module('lscsClientApp')
         restrict: 'E',
         scope: {
             employee:"="
-        },
-        controller: ['$scope', function($scope) {
-
-        }]
+        }
     };
   });
